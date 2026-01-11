d7d877a27e17d881ce6ccc5bb6e87b1e95b1e2c6
Ryan Girard <rmg9923@gmail.com>
2026-01-10 20:09:42 -0500

    grid generator logic cont.

--- src/app/utils/gridGenerator.js
+++ src/app/utils/gridGenerator.js
@@ -8,16 +8,24 @@ const gridGenerator = () => {
     const consonants = LETTERS.filter(letters => {
         return letters.vowel !== true
     })
-
     const getRandomLetter = () => {
         const chosenLetters = Math.random() < 0.4 ? vowels : consonants
         const randomChosen = Math.floor(Math.random() * chosenLetters.length)
         return chosenLetters[randomChosen]
-}
-console.log(vowels)
-console.log(consonants)
-console.log(getRandomLetter())
-
+    }
+    const grid = []
+    for (let row = 0; row < 6; row++) {
+        const rowArray = []
+        for (let col = 0; col < 6; col++) {
+            const randomLetter = getRandomLetter()
+            rowArray.push(randomLetter)
+        }
+        grid.push(rowArray)
+    }
+    console.log(grid)
+    console.log(vowels)
+    console.log(consonants)
+    console.log(getRandomLetter())
 }
 
 export default gridGenerator
