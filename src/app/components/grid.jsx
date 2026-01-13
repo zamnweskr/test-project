a7b83f79c09130192937e09e2d39222de5f6fcdc
Ryan Girard <rmg9923@gmail.com>
2026-01-13 11:44:42 -0500

    created mapping logic to insert letters into grid, did some CSS to make it look appealing

--- src/app/components/grid.jsx
+++ src/app/components/grid.jsx
@@ -1,12 +1,23 @@
 import gridGenerator from "../utils/gridGenerator"
+import './grid.css'
 
 function Grid({ grid }) {
     console.log(grid)
-  return (
-    <div>
-      <h1>Grid Component</h1>
-    </div>
-  )
+    return (
+        <div className='grid-container'>
+            {
+                grid.map((row, rowID) => {
+                    return (
+                        <div className='grid-row' key={rowID}>{row.map((letter) => {
+                            return (
+                                <img className='letter-tile' key={letter.id} src={letter.src} alt={letter.name}/>
+                            )
+                        })}</div>
+                    )
+                })
+            }
+        </div>
+    )
 }
 
 export default Grid
