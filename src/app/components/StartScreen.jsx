f8f6b67f81768cb3150d42cc74aad4115522cc00
Ryan Girard <rmg9923@gmail.com>
2026-01-31 11:28:05 -0500

    added directions to start page

--- src/app/components/StartScreen.jsx
+++ src/app/components/StartScreen.jsx
@@ -1,15 +1,17 @@
-import React from 'react';
-
 const StartScreen = ({ onStartGame }) => {
-  return (
-    <div className="start-screen">
-      <h1 className={"start-title"}>A Game About WORDS !</h1>
-      <p>Can you find your way to a win?</p>
-      <button onClick={onStartGame}>
-        Start Game
-      </button>
-    </div>
-  );
+    return (
+        <div className="start-screen">
+            <h1 className={"start-title"}>WORD?!</h1>
+            <p>Create the words on screen by clicking the letters to rearrange them. Match all 5 words before the time runs out!
+            <br />
+            <br />
+                Move letters by swapping them with adjacent letters to form words, letters can be arranged horizontally or vertically, but not diagonally!
+            </p>
+            <button onClick={onStartGame}>
+                Start Game
+            </button>
+        </div>
+    );
 };
 
 
